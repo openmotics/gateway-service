ae2e616245a3e7aaf3602d6c918d55042a170032
Kenneth Henderick <kenneth@ketronic.be>
2020-03-13 12:26:43 +0100

    The pip install of wheels should not use /tmp since memory/space is limited.

--- src/post-update.sh
+++ src/post-update.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 OS_DIST=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release`
-python /opt/openmotics/python/libs/pip.whl/pip install --upgrade --target /opt/openmotics/dist-packages --no-index /opt/openmotics/python/libs/$OS_DIST/*.whl
+TEMP_DIR=$(mktemp -d -t ci-XXXXXXXX --tmpdir=/opt/openmotics)
+env TMPDIR=$TEMP_DIR python /opt/openmotics/python/libs/pip.whl/pip install --upgrade --target /opt/openmotics/dist-packages --no-index /opt/openmotics/python/libs/$OS_DIST/*.whl
+rm -rf $TEMP_DIR
