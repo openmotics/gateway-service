0e5d6cbee17a49d369a558aeebee388ecf9284c2
Maarten De Meyer <mmeye@renson.be>
2020-04-06 15:59:06 +0200

    Use python3 in run3.sh

--- testing/unittests/run3.sh
+++ testing/unittests/run3.sh
@@ -2,8 +2,8 @@
 export PYTHONPATH=$PYTHONPATH:`pwd`/../../src
 
 echo "Running master api tests"
-python2 master_tests/master_api_tests.py
+python3 master_tests/master_api_tests.py
 
 echo "Running master command tests"
-python2 master_tests/master_command_tests.py
+python3 master_tests/master_command_tests.py
 
