8c3b38905868ced9c00b8b1908f97146c279b56b
Kenneth Henderick <kenneth@ketronic.be>
2020-04-17 09:33:45 +0200

    Discovered the `-p` parameter

--- src/post-update.sh
+++ src/post-update.sh
@@ -2,8 +2,6 @@
 set -e
 
 OS_DIST=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release`
-DATE=`date +%Y%m%d%H%M%S`
-TEMP_DIR="/opt/openmotics/tmp_$DATE"
-mkdir $TEMP_DIR
+TEMP_DIR=$(mktemp -d -t pip-XXXXXXXX -p /opt/openmotics)
 env TMPDIR=$TEMP_DIR PYTHONUSERBASE=/opt/openmotics/python-deps python2 /opt/openmotics/python/libs/pip.whl/pip install --no-index --user /opt/openmotics/python/libs/$OS_DIST/*.whl
 rm -rf $TEMP_DIR
