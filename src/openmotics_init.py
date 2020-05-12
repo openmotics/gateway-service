4da249c280ab7b7aa1463f7b2f8bad61da3a533e
Daiderd Jordan <djordan@openmotics.com>
2020-05-12 16:13:50 +0200

    move platform setup out of openmotics_init.py

--- src/openmotics_init.py
+++ src/openmotics_init.py
@@ -16,19 +16,17 @@
 The main module for the OpenMotics
 """
 from __future__ import absolute_import
-from platform_utils import Platform, System
 
+from platform_utils import System
 System.import_libs()
 
 import logging
 import os
 import time
-from six.moves.configparser import ConfigParser
-from threading import Lock
 
 import constants
-from ioc import INJECTED, Inject, Injectable
-from serial import Serial
+from ioc import INJECTED, Inject
+from master import setup_platform
 
 logger = logging.getLogger("openmotics")
 
@@ -80,27 +78,7 @@ def factory_reset(master_controller=INJECTED):
 
 if __name__ == '__main__':
     setup_logger()
-
-    config = ConfigParser()
-    config.read(constants.get_config_file())
-
-    Injectable.value(config_db_lock=Lock())
-    Injectable.value(config_db=constants.get_config_database_file())
-    Injectable.value(eeprom_db=constants.get_eeprom_extension_database_file())
-
-    controller_serial_port = config.get('OpenMotics', 'controller_serial')
-    Injectable.value(controller_serial=Serial(controller_serial_port, 115200))
-
-    from gateway import config as config_controller
-    _ = config_controller
-    if Platform.get_platform() == Platform.Type.CORE_PLUS:
-        from gateway.hal import master_controller_core  # type: ignore
-        from master.core import maintenance, core_communicator, ucan_communicator  # type: ignore
-        _ = master_controller_core, maintenance, core_communicator, ucan_communicator  # type: ignore
-    else:
-        from gateway.hal import master_controller_classic  # type: ignore
-        from master.classic import maintenance, master_communicator, eeprom_extension  # type: ignore
-        _ = master_controller_classic, maintenance, master_communicator, eeprom_extension  # type: ignore
+    setup_platform()
 
     lock_file = constants.get_init_lockfile()
     if os.path.isfile(lock_file):
