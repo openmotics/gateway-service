92936b8a49437b5b1b0ada929dcf1a62389a4d33
Daiderd Jordan <djordan@openmotics.com>
2020-05-19 13:32:58 +0200

    add coverage reporting to unittests

--- testing/unittests/run.sh
+++ testing/unittests/run.sh
@@ -6,4 +6,7 @@ cd "$(dirname "${BASH_SOURCE[0]}")/../.."
 # TODO install egglink instead
 export PYTHONPATH=$PYTHONPATH:$PWD/src
 
-pytest testing/unittests --log-level=DEBUG --durations=2 --junit-xml testing/gw-unit-reports/gateway.xml
+pytest testing/unittests --log-level=DEBUG --durations=2 \
+    --junit-xml testing/gw-unit-reports/gateway.xml \
+    --cov-report xml --cov-fail-under=50 \
+    --cov=bus --cov=gateway --cov=ioc --cov=master --cov=serial_utils --cov=toolbox
