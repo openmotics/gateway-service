e3e520c1d8a7d800d330e949f5f28248b6390cf7
Kenneth Henderick <kenneth@ketronic.be>
2020-06-15 13:40:12 +0200

    Align code with general structure changes on develop

--- testing/cicd/tests/energy_module_test.py
+++ testing/cicd/tests/energy_module_test.py
@@ -17,62 +17,43 @@ import logging
 import hypothesis
 import pytest
 import time
-from hypothesis.strategies import composite, just, one_of
-from six.moves import map
+from tests.hardware import cts, CT
 
 if False:
-    from typing import Tuple, Callable
     from .toolbox import Toolbox
 
 logger = logging.getLogger('openmotics')
 
 
-@composite
-def next_ct(draw):
-    used_values = []
-
-    def f(toolbox):  # type: (Toolbox) -> Tuple[int, int]
-        assert len(toolbox.dut_energy_cts), 'no Energy modules available for this test'
-        elements = [toolbox.dut_energy_cts[0]]  # For now, only the first CT is installed
-        value = draw(one_of(list(map(just, elements))).
-                     filter(lambda x: (x not in used_values)))
-        used_values.append(value)
-        hypothesis.note('module e#{}.{}'.format(*value))
-        return value
-    return f
-
-
 @pytest.mark.smoke
 @pytest.mark.skip(reason='Unstable module discovery')
-@hypothesis.given(next_ct())
-def test_realtime_power(toolbox, next_ct):  # type: (Toolbox, Callable[[Toolbox], Tuple[int, int]]) -> None
-    module_id, input_id = next_ct(toolbox)
-    _assert_realtime(toolbox, module_id, input_id)
+@hypothesis.given(cts())
+def test_realtime_power(toolbox, ct):  # type: (Toolbox, CT) -> None
+    _assert_realtime(toolbox, ct)
 
 
 @pytest.mark.slow
 @pytest.mark.skip(reason='Unstable module discovery')
-@hypothesis.given(next_ct())
-def test_power_cycle(toolbox, next_ct):  # type: (Toolbox, Callable[[Toolbox], Tuple[int, int]]) -> None
-    module_id, input_id = next_ct(toolbox)
+@hypothesis.given(cts())
+def test_power_cycle(toolbox, ct):  # type: (Toolbox, CT) -> None
     cycles = 10
     post_boot_wait = 5  # Wait `post_boot_wait` seconds after powering up the module to start using it
     toolbox.set_output(toolbox.POWER_ENERGY_MODULE, True)
     time.sleep(post_boot_wait)
-    _assert_realtime(toolbox, module_id, input_id)
+    _assert_realtime(toolbox, ct)
     with toolbox.disabled_self_recovery():
         for cycle in range(cycles):
-            logger.info('power cycle energy module e#{} ({}/{})'.format(module_id, cycle + 1, cycles))
+            logger.info('power cycle energy module e#{} ({}/{})'.format(ct.module_id, cycle + 1, cycles))
             toolbox.power_cycle_module(toolbox.POWER_ENERGY_MODULE)
             time.sleep(post_boot_wait)
-            _assert_realtime(toolbox, module_id, input_id)
+            _assert_realtime(toolbox, ct)
 
 
-def _assert_realtime(toolbox, module_id, input_id):  # type: (Toolbox, int, int) -> None
-    logger.info('validating realtime data from energy module e#{}.{}'.format(module_id, input_id))
+def _assert_realtime(toolbox, ct):  # type: (Toolbox, CT) -> None
+    logger.info('validating realtime data from energy module e#{}.{}'.format(ct.module_id, ct.ct_id))
     data = toolbox.dut.get('/get_realtime_power')
-    assert str(module_id) in data
-    voltage, frequency, current, power = data[str(module_id)][input_id]
+    assert str(ct.module_id) in data
+    voltage, frequency, current, power = data[str(ct.module_id)][ct.ct_id]
     assert 220 <= voltage <= 240
     assert 49 <= frequency <= 51
     assert 20 <= power <= 40
