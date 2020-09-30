b420721ac8931d63aec8b496afce96c3db5f2c4d
Kenneth Henderick <kenneth@ketronic.be>
2020-09-30 13:08:27 +0200

    Updated `run3.sh` script to execute all tests at once

--- testing/unittests/run3.sh
+++ testing/unittests/run3.sh
@@ -7,17 +7,19 @@ declare -a blacklist=(
     gateway_tests/scheduling_test.py
     gateway_tests/shutter_test.py
     gateway_tests/webservice_test.py
+    gateway_tests/metrics_test.py
     plugins_tests/base_test.py
     plugins_tests/runner_test.py
 )
 
-find . -name '*_test.py' | while read -r f; do
+files=""
+for f in $(find . -name '*_test.py'); do
     f=${f#./*}
     if [[ "${blacklist[*]}" =~ "$f" ]]; then
         echo "Skipping $f..." >&2
         continue
     fi
-    out=${f//\//_}
-    echo "Testing $f..." >&2
-    pytest "$f" --log-level=DEBUG --durations=2 --junit-xml "../gw-unit-reports-3/$out.xml" || true
+    files="$files $f"
 done
+
+pytest $files --log-level=DEBUG --durations=2 --junit-xml "../gw-unit-reports-3/gateway.xml"
