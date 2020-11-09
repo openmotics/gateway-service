c8156540610e7ae357c049b4841967987f51ea19
Kenneth Henderick <kenneth@ketronic.be>
2020-11-09 08:15:43 +0100

    Allow longer pulse counter names

--- src/gateway/hal/mappers_classic/pulse_counter.py
+++ src/gateway/hal/mappers_classic/pulse_counter.py
@@ -41,7 +41,10 @@ class PulseCounterMapper(object):
     def dto_to_orm(pulse_counter_dto, fields):  # type: (PulseCounterDTO, List[str]) -> EepromModel
         data = {'id': pulse_counter_dto.id}  # type: Dict[str, Any]
         if 'name' in fields:
-            data['name'] = pulse_counter_dto.name
+            name = pulse_counter_dto.name
+            if len(name) > 16:
+                name = '{0}~'.format(name[:15])
+            data['name'] = name
         if 'input_id' in fields:
             data['input'] = Toolbox.denonify(pulse_counter_dto.input_id, PulseCounterMapper.BYTE_MAX)
         return PulseCounterConfiguration.deserialize(data)
