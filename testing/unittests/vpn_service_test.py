3f7af8ad88897855bd42fdbec1ad1808b83c9d65
Kenneth Henderick <kenneth@ketronic.be>
2021-01-19 08:43:20 +0100

    Fix heartbeat test

--- testing/unittests/vpn_service_test.py
+++ testing/unittests/vpn_service_test.py
@@ -354,11 +354,8 @@ class VPNServiceTest(TestCase):
                     self.assertEqual(2, heartbeat._sleep_time)
                     self.assertEqual(now, heartbeat._last_successful_heartbeat)
                     post.assert_called_once_with('https://foobar',
-                                                 data={'extra_data': json.dumps({'inputs': 'inputscoll',
-                                                                                 'errors': 'errorscoll',
+                                                 data={'extra_data': json.dumps({'errors': 'errorscoll',
                                                                                  'local_ip': 'ip addresscoll',
-                                                                                 'thermostats': 'thermostatscoll',
-                                                                                 'shutters': 'shutterscoll',
                                                                                  'debug': {}},
                                                                                 sort_keys=True)},
                                                  timeout=10.0)
