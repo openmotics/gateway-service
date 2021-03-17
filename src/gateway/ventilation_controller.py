054fd8100ece630c081a8611b669d33a6b9e16bf
Pieter-Jan Tuytschaever <pjtuytsc@openmotics.com>
2021-03-17 14:26:48 +0100

    Add timer value to the output of the gateway api

--- src/gateway/ventilation_controller.py
+++ src/gateway/ventilation_controller.py
@@ -65,6 +65,11 @@ class VentilationController(object):
 
     def _publish_state(self, state_dto):
         # type: (VentilationStatusDTO) -> None
+        # if the timer or remaining time is set, the other value will not be set,
+        # so cache the previous value so it does not get lost
+        if self.last_ventilation_status.get(state_dto.id) is not None:
+            state_dto.timer = state_dto.timer or self.last_ventilation_status[state_dto.id].timer
+            state_dto.remaining_time = state_dto.remaining_time or self.last_ventilation_status[state_dto.id].remaining_time
         self.last_ventilation_status[state_dto.id] = state_dto
         event_data = {'id': state_dto.id,
                       'mode': state_dto.mode,
