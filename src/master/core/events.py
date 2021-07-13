10ef66912202ab0b48c43edf6b8561fab78a00ae
Kenneth Henderick <kenneth@ketronic.be>
2021-07-13 09:46:30 +0200

    Reflecting event changes from Brain firmware >= 1.0.103

--- src/master/core/events.py
+++ src/master/core/events.py
@@ -23,7 +23,7 @@ from master.core.system_value import Temperature, Humidity, Timer
 from master.core.basic_action import BasicAction
 
 if False:  # MYPY
-    from typing import List, Optional
+    from typing import List
 
 logger = logging.getLogger(__name__)
 
@@ -37,6 +37,8 @@ class Event(object):
         SYSTEM = 'SYSTEM'
         POWER = 'POWER'
         EXECUTED_BA = 'EXECUTED_BA'
+        RESET_ACTION = 'RESET_ACTION'
+        GENERIC_DATA = 'GENERIC_DATA'
         BUTTON_PRESS = 'BUTTON_PRESS'
         LED_ON = 'LED_ON'
         LED_BLINK = 'LED_BLINK'
@@ -44,7 +46,7 @@ class Event(object):
         EXECUTE_GATEWAY_API = 'EXECUTE_GATEWAY_API'
         UNKNOWN = 'UNKNOWN'
 
-    class OutputEventTypes(object):
+    class IOEventTypes(object):
         STATUS = 'STATUS'
         LOCKING = 'LOCKING'
 
@@ -61,11 +63,41 @@ class Event(object):
         TEMPERATURE = 'TEMPERATURE'
         HUMIDITY = 'HUMIDITY'
         BRIGHTNESS = 'BRIGHTNESS'
+        CO2 = 'CO2'
+        VOC = 'VOC'
         UNKNOWN = 'UNKNOWN'
 
     class SystemEventTypes(object):
         EEPROM_ACTIVATE = 'EEPROM_ACTIVATE'
-        ONBOARD_TEMP_CHANGED = 'ONBOARD_TEMP_CHANGED'
+        I2C_RESET = 'I2C_RESET'
+        HEALTH = 'HEALTH'
+        UNKNOWN = 'UNKNOWN'
+
+    class GenericDataTypes(object):
+        PCB_TEMPERATURE = 'PCB_TEMPERATURE'
+        UNKNOWN = 'UNKNOWN'
+
+    class SystemHealthTypes(object):
+        CAN = 'CAN'
+        UNKNOWN = 'UNKNOWN'
+
+    class CANHealthTypes(object):
+        NO_UCAN_OK = 'NO_UCAN_OK'
+        ALL_UCAN_OK = 'ALL_UCAN_OK'
+        SOME_UCAN_OK = 'SOME_UCAN_OK'
+        CAN_BUS_OFF = 'CAN_BUS_OFF'
+        UNKNOWN = 'UNKNOWN'
+
+    class ResetTypes(object):
+        HEALTH_CHECK = 'HEALTH_CHECK'
+        CAN_STACK = 'CAN_STACK'
+        PROCESSOR_RESET = 'PROCESSOR_RESET'
+        UNKNOWN = 'UNKNOWN'
+
+    class HealthResetCauses(object):
+        I2C_1 = 'I2C_1'
+        I2C_2 = 'I2C_2'
+        CAN = 'CAN'
         UNKNOWN = 'UNKNOWN'
 
     class ThermostatOrigins(object):
@@ -133,12 +165,14 @@ class Event(object):
                     20: Event.Types.THERMOSTAT,
                     21: Event.Types.UCAN,
                     22: Event.Types.EXECUTED_BA,
+                    247: Event.Types.GENERIC_DATA,
+                    248: Event.Types.SYSTEM,
                     249: Event.Types.EXECUTE_GATEWAY_API,
                     250: Event.Types.BUTTON_PRESS,
                     251: Event.Types.LED_BLINK,
                     252: Event.Types.LED_ON,
                     253: Event.Types.POWER,
-                    254: Event.Types.SYSTEM}
+                    254: Event.Types.RESET_ACTION}
         return type_map.get(self._type, Event.Types.UNKNOWN)
 
     @property
@@ -149,32 +183,41 @@ class Event(object):
                 timer_type = self._data[1]  # type: int
                 timer_value = self._word_decode(self._data[2:]) or 0  # type: int
                 timer = Timer.event_timer_type_to_seconds(timer_type, timer_value)
-                data.update({'type': Event.OutputEventTypes.STATUS,
+                data.update({'type': Event.IOEventTypes.STATUS,
                              'status': self._action == 1,
                              'dimmer_value': self._data[0],
                              'timer': timer})
             else:
-                data.update({'type': Event.OutputEventTypes.LOCKING,
+                data.update({'type': Event.IOEventTypes.LOCKING,
                              'locked': self._data[0] == 1})
             return data
         if self.type == Event.Types.INPUT:
-            return {'input': self._device_nr,
-                    'status': self._action == 1}
+            data = {'input': self._device_nr}
+            if self._action in [0, 1]:
+                data.update({'type': Event.IOEventTypes.STATUS,
+                             'status': self._action == 1})
+            else:
+                data.update({'type': Event.IOEventTypes.LOCKING,
+                             'locked': self._data[0] == 1})
+            return data
         if self.type == Event.Types.SENSOR:
-            sensor_type = Event.SensorType.UNKNOWN
-            sensor_value = None
+            sensor_values = []
             if self._action == 0:
-                sensor_type = Event.SensorType.TEMPERATURE
-                sensor_value = Temperature.system_value_to_temperature(self._data[1])
+                sensor_values += [{'type': Event.SensorType.TEMPERATURE,
+                                   'value': Temperature.system_value_to_temperature(self._data[1])}]
             elif self._action == 1:
-                sensor_type = Event.SensorType.HUMIDITY
-                sensor_value = Humidity.system_value_to_humidity(self._data[1])
+                sensor_values += [{'type': Event.SensorType.HUMIDITY,
+                                   'value': Humidity.system_value_to_humidity(self._data[1])}]
             elif self._action == 2:
-                sensor_type = Event.SensorType.BRIGHTNESS
-                sensor_value = self._word_decode(self._data[0:2])
+                sensor_values += [{'tye': Event.SensorType.BRIGHTNESS,
+                                   'value': self._word_decode(self._data[0:2])}]
+            elif self._action == 3:
+                sensor_values += [{'type': Event.SensorType.CO2,
+                                   'value': self._word_decode(self._data[0:2])},
+                                  {'type': Event.SensorType.VOC,
+                                   'value': self._word_decode(self._data[2:4])}]
             return {'sensor': self._device_nr,
-                    'type': sensor_type,
-                    'value': sensor_value}
+                    'values': sensor_values}
         if self.type == Event.Types.THERMOSTAT:
             origin_map = {0: Event.ThermostatOrigins.SLAVE,
                           1: Event.ThermostatOrigins.MASTER}
@@ -182,6 +225,60 @@ class Event(object):
                     'thermostat': self._device_nr,
                     'mode': self._data[0],
                     'setpoint': self._data[1]}
+        if self.type == Event.Types.UCAN:
+            data = {'address': self._address_helper.decode(bytearray([self._device_nr & 0xFF]) + self._data[0:2])}
+            if self._data[2] == 0 and self._data[3] == 0:
+                data['type'] = Event.UCANEventTypes.POWER_OUT_ERROR
+            elif self._data[2] == 0 and self._data[3] == 1:
+                data['type'] = Event.UCANEventTypes.POWER_OUT_RESTORED
+            elif self._data[2] == 0 and self._data[3] == 2:
+                data['type'] = Event.UCANEventTypes.POWER_OUT_OFF
+            elif self._data[2] == 2 and self._data[3] == 3:
+                data['type'] = Event.UCANEventTypes.POWER_OUT_ON
+            elif self._data[2] == 3:
+                data['type'] = Event.UCANEventTypes.RESTARTED
+            elif self._data[2] == 1:
+                data.update({'type': Event.UCANEventTypes.I2C_ERROR,
+                             'i2c_address': self._data[3]})
+            else:
+                data.update({'type': Event.UCANEventTypes.UNKNOWN,
+                             'data': self._data[2:4]})
+            return data
+        if self.type == Event.Types.EXECUTED_BA:
+            return {'basic_action': BasicAction(action_type=self._data[0],
+                                                action=self._data[1],
+                                                device_nr=self._device_nr,
+                                                extra_parameter=self._word_decode(self._data[2:4]))}
+        if self.type == Event.Types.GENERIC_DATA:
+            if self._action == 0:
+                return {'action': Event.GenericDataTypes.PCB_TEMPERATURE,
+                        'temperature': (self._device_nr >> 8) & 0xFF}
+            return {'action': Event.GenericDataTypes.UNKNOWN,
+                    'device_nr': self._device_nr,
+                    'data': self._data}
+        if self.type == Event.Types.SYSTEM:
+            if self._action == 0:
+                return {'type': Event.SystemEventTypes.EEPROM_ACTIVATE}
+            if self._action == 2:
+                return {'type': Event.SystemEventTypes.I2C_RESET,
+                        'cause': self._data[0]}
+            if self._action == 3:
+                data = {'type': Event.SystemEventTypes.HEALTH,
+                        'health_type': Event.SystemHealthTypes.UNKNOWN}
+                if self._data[0] == 0:
+                    can_health_map = {1: Event.CANHealthTypes.NO_UCAN_OK,
+                                      2: Event.CANHealthTypes.ALL_UCAN_OK,
+                                      3: Event.CANHealthTypes.SOME_UCAN_OK,
+                                      4: Event.CANHealthTypes.CAN_BUS_OFF}
+                    data.update({'health_type': Event.SystemHealthTypes.CAN,
+                                 'current': can_health_map.get(self._data[1], Event.CANHealthTypes.UNKNOWN),
+                                 'previous': can_health_map.get(self._data[2], Event.CANHealthTypes.UNKNOWN)})
+                return data
+            return {'type': Event.SystemEventTypes.UNKNOWN}
+        if self.type == Event.Types.EXECUTE_GATEWAY_API:
+            return {'action': self._data[3],
+                    'device_nr': self._device_nr,
+                    'extra_parameter': self._word_decode(self._data[0:2])}
         if self.type == Event.Types.BUTTON_PRESS:
             return {'button': self._device_nr,
                     'state': self._data[0]}
@@ -211,42 +308,19 @@ class Event(object):
         if self.type == Event.Types.POWER:
             return {'bus': Event.Bus.RS485 if self._device_nr == 0 else Event.Bus.CAN,
                     'power': self._data[0 > 1]}
-        if self.type == Event.Types.SYSTEM:
-            type_map = {0: Event.SystemEventTypes.EEPROM_ACTIVATE,
-                        1: Event.SystemEventTypes.ONBOARD_TEMP_CHANGED}
-            event_type = type_map.get(self._action, Event.SystemEventTypes.UNKNOWN)
-            event_data = {'type': event_type}
-            if event_type == Event.SystemEventTypes.ONBOARD_TEMP_CHANGED:
-                event_data['temperature'] = self._data[0]
-            return event_data
-        if self.type == Event.Types.UCAN:
-            event_data = {'address': self._address_helper.decode(bytearray([self._device_nr & 0xFF]) + self._data[0:2])}
-            if self._data[2] == 0 and self._data[3] == 0:
-                event_data['type'] = Event.UCANEventTypes.POWER_OUT_ERROR
-            elif self._data[2] == 0 and self._data[3] == 1:
-                event_data['type'] = Event.UCANEventTypes.POWER_OUT_RESTORED
-            elif self._data[2] == 0 and self._data[3] == 2:
-                event_data['type'] = Event.UCANEventTypes.POWER_OUT_OFF
-            elif self._data[2] == 2 and self._data[3] == 3:
-                event_data['type'] = Event.UCANEventTypes.POWER_OUT_ON
-            elif self._data[2] == 3:
-                event_data['type'] = Event.UCANEventTypes.RESTARTED
-            elif self._data[2] == 1:
-                event_data.update({'type': Event.UCANEventTypes.I2C_ERROR,
-                                   'i2c_address': self._data[3]})
-            else:
-                event_data.update({'type': Event.UCANEventTypes.UNKNOWN,
-                                   'data': self._data[2:4]})
-            return event_data
-        if self.type == Event.Types.EXECUTED_BA:
-            return {'basic_action': BasicAction(action_type=self._data[0],
-                                                action=self._data[1],
-                                                device_nr=self._device_nr,
-                                                extra_parameter=self._word_decode(self._data[2:4]))}
-        if self.type == Event.Types.EXECUTE_GATEWAY_API:
-            return {'action': self._data[3],
-                    'device_nr': self._device_nr,
-                    'extra_parameter': self._word_decode(self._data[0:2])}
+        if self.type == Event.Types.RESET_ACTION:
+            data = {'type': Event.ResetTypes.UNKNOWN,
+                    'address': self._address_helper.decode(self._data[1:4])}
+            if self._action == 2:
+                data.update({'type': Event.ResetTypes.HEALTH_CHECK,
+                             'cause': {1: Event.HealthResetCauses.I2C_1,
+                                       2: Event.HealthResetCauses.I2C_2,
+                                       3: Event.HealthResetCauses.CAN}.get(self._data[0], Event.HealthResetCauses.UNKNOWN)})
+            elif self._action == 253:
+                data.update({'type': Event.ResetTypes.CAN_STACK})
+            elif self._action == 254:
+                data.update({'type': Event.ResetTypes.PROCESSOR_RESET})
+            return data
         return None
 
     def _word_decode(self, data):  # type: (List[int]) -> int
