cf631ccaf12d628b4caab323ba6184c137381a7d
Sander Van Schoote <vanschoote.sander@gmail.com>
2021-07-30 12:43:03 +0200

    Return newly created sensors upon API request

--- src/gateway/webservice.py
+++ src/gateway/webservice.py
@@ -1414,15 +1414,17 @@ class WebInterface(object):
     def set_sensor_configuration(self, config):  # type: (Dict[Any, Any]) -> Dict
         """ Set one sensor_configuration. """
         data = SensorSerializer.deserialize(config)
-        self._sensor_controller.save_sensors([data])
-        return {}
+        saved_sensors_dtos = self._sensor_controller.save_sensors([data])
+        return {'config': [SensorSerializer.serialize(sensor_dto=saved_sensors_dto, fields=None)
+                           for saved_sensors_dto in saved_sensors_dtos]}
 
     @openmotics_api(auth=True, check=types(config='json'))
     def set_sensor_configurations(self, config):  # type: (List[Dict[Any, Any]]) -> Dict
         """ Set multiple sensor_configurations. """
         data = [SensorSerializer.deserialize(entry) for entry in config]
-        self._sensor_controller.save_sensors(data)
-        return {}
+        saved_sensors_dtos = self._sensor_controller.save_sensors(data)
+        return {'config': [SensorSerializer.serialize(sensor_dto=saved_sensors_dto, fields=None)
+                           for saved_sensors_dto in saved_sensors_dtos]}
 
     # Heating Pump Group
 
