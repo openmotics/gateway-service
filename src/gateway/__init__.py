119cc74d9102753b1cd3930c466615355289656c
Kenneth Henderick <khenderi@openmotics.com>
2021-08-23 10:52:44 +0200

    Version bump

--- src/gateway/__init__.py
+++ src/gateway/__init__.py
@@ -14,4 +14,4 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """ Module containing the functionality provided by the gateway. """
 
-__version__ = '2.18.1'
+__version__ = '2.18.2'
