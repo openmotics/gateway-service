c4a70fb883d2806956dc95d6171879a45523447e
Pieter-Jan Tuytschaever <pjtuytsc@openmotics.com>
2021-11-30 15:12:23 +0100

    Fix pubsub mypy checks

--- src/gateway/pubsub.py
+++ src/gateway/pubsub.py
@@ -21,13 +21,13 @@ from six.moves.queue import Queue, Empty
 
 from gateway.daemon_thread import DaemonThread
 from gateway.enums import BaseEnum
+from gateway.events import GatewayEvent, EsafeEvent
+from gateway.hal.master_event import MasterEvent
 
 from ioc import Injectable, Singleton
 
 if False:  # MYPY
-    from typing import Callable, Dict, List, Literal, Tuple
-    from gateway.events import GatewayEvent, EsafeEvent
-    from gateway.hal.master_event import MasterEvent
+    from typing import Callable, Dict, List, Literal, Tuple, Union
     GATEWAY_TOPIC = Literal['config', 'state']
     MASTER_TOPIC = Literal['eeprom', 'module', 'power', 'output', 'input', 'shutter', 'sensor']
     ESAFE_TOPIC = Literal['delivery', 'lock', 'config', 'rfid']
@@ -36,14 +36,13 @@ logger = logging.getLogger(__name__)
 
 
 class EventStructure(object):
-    def __init__(self, namespace, topic, event):
-        # type: (PubSub.EventNamespace, PubSub.EventTopic, object) -> None
-        self.namespace = namespace
+    def __init__(self, topic, event):
+        # type: (Union[MASTER_TOPIC, GATEWAY_TOPIC, ESAFE_TOPIC], Union[MasterEvent, GatewayEvent, EsafeEvent]) -> None
         self.topic = topic
         self.event = event
 
     def __repr__(self):
-        return "<EventStructure: {}: {}: {}>".format(self.namespace, self.topic, self.event)
+        return "<EventStructure: Topic: {}, Event: {}>".format(self.topic, self.event)
 
     def __str__(self):
         return self.__repr__()
@@ -56,32 +55,23 @@ class PubSub(object):
     This module will act as an event broker that will distribute events all across the gateway code where required.
 
     The pubsub module will require the following data to fully publish an event:
-     - Namespace: ['MASTER', 'GATEWAY', 'ESAFE']
      - Topic: The specific topic that is provided for the event in a particular namespace.
      - Type: the type of event that is defined in the event itself.
     The above list can is ordered in hierarchical order.
     """
 
-    class EventNamespace(BaseEnum):
-        MASTER = 'MASTER'
-        GATEWAY = 'GATEWAY'
-        ESAFE = 'ESAFE'
-
-    class EventTopic(BaseEnum):
-        pass
-
-    class MasterTopics(EventTopic):
+    class MasterTopics(BaseEnum):
         EEPROM = 'eeprom'  # type: MASTER_TOPIC
         OUTPUT = 'output'  # type: MASTER_TOPIC
         INPUT = 'input'  # type: MASTER_TOPIC
         SHUTTER = 'shutter'  # type: MASTER_TOPIC
         SENSOR = 'sensor'  # type: MASTER_TOPIC
 
-    class GatewayTopics(EventTopic):
+    class GatewayTopics(BaseEnum):
         CONFIG = 'config'  # type: GATEWAY_TOPIC
         STATE = 'state'  # type: GATEWAY_TOPIC
 
-    class EsafeTopics(EventTopic):
+    class EsafeTopics(BaseEnum):
         DELIVERY = 'delivery'   # type: ESAFE_TOPIC
         LOCK = 'lock'           # type: ESAFE_TOPIC
         CONFIG = 'config'       # type: ESAFE_TOPIC
@@ -124,12 +114,17 @@ class PubSub(object):
 
     def _publish_event(self, event_structure):
         # type: (EventStructure) -> None
-        if event_structure.namespace == PubSub.EventNamespace.MASTER:
-            self._publish_master_event(event_structure.topic, event_structure.event)
-        elif event_structure.namespace == PubSub.EventNamespace.GATEWAY:
-            self._publish_gateway_event(event_structure.topic, event_structure.event)
-        elif event_structure.namespace == PubSub.EventNamespace.ESAFE:
-            self._publish_esafe_event(event_structure.topic, event_structure.event)
+        if isinstance(event_structure.event, MasterEvent):
+            master_topic = event_structure.topic  # type: MASTER_TOPIC  # type: ignore
+            self._publish_master_event(master_topic, event_structure.event)
+        elif isinstance(event_structure.event, GatewayEvent):
+            # elif event_structure.namespace == PubSub.EventNamespace.GATEWAY:
+            gateway_topic = event_structure.topic  # type: GATEWAY_TOPIC  # type: ignore
+            self._publish_gateway_event(gateway_topic, event_structure.event)
+        elif isinstance(event_structure.event, EsafeEvent):
+            # elif event_structure.namespace == PubSub.EventNamespace.ESAFE:
+            esafe_topic = event_structure.topic  # type: ESAFE_TOPIC  # type: ignore
+            self._publish_esafe_event(esafe_topic, event_structure.event)
 
     def subscribe_master_events(self, topic, callback):
         # type: (MASTER_TOPIC, Callable[[MasterEvent],None]) -> None
@@ -137,11 +132,11 @@ class PubSub(object):
 
     def publish_master_event(self, topic, master_event):
         # type: (MASTER_TOPIC, MasterEvent) -> None
-        self._event_queue.put(EventStructure(PubSub.EventNamespace.MASTER, topic, master_event))
+        self._event_queue.put(EventStructure(topic, master_event))
         # self._master_events.put((topic, master_event))
 
     def _publish_master_event(self, topic, master_event):
-        # type: (MASTER_TOPIC, object) -> None
+        # type: (MASTER_TOPIC, MasterEvent) -> None
         callbacks = self._master_topics[topic]
         if callbacks:
             logger.debug('Received master event %s on topic "%s"', master_event.type, topic)
@@ -159,10 +154,10 @@ class PubSub(object):
 
     def publish_gateway_event(self, topic, gateway_event):
         # type: (GATEWAY_TOPIC, GatewayEvent) -> None
-        self._event_queue.put(EventStructure(PubSub.EventNamespace.GATEWAY, topic, gateway_event))
+        self._event_queue.put(EventStructure(topic, gateway_event))
 
     def _publish_gateway_event(self, topic, gateway_event):
-        # type: (GATEWAY_TOPIC, object) -> None
+        # type: (GATEWAY_TOPIC, GatewayEvent) -> None
         logger.debug('Publishing gateway event {} {}'.format(topic, gateway_event))
         callbacks = self._gateway_topics[topic]
         if callbacks:
@@ -182,10 +177,10 @@ class PubSub(object):
 
     def publish_esafe_event(self, topic, esafe_event):
         # type: (ESAFE_TOPIC, EsafeEvent) -> None
-        self._event_queue.put(EventStructure(PubSub.EventNamespace.ESAFE, topic, esafe_event))
+        self._event_queue.put(EventStructure(topic, esafe_event))
 
     def _publish_esafe_event(self, topic, esafe_event):
-        # type: (ESAFE_TOPIC, object) -> None
+        # type: (ESAFE_TOPIC, EsafeEvent) -> None
         logger.debug('Publishing esafe event {} {}'.format(topic, esafe_event))
         callbacks = self._esafe_topics[topic]
         if callbacks:
