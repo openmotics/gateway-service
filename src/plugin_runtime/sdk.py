cf3e3c864134a2112d2649e03524276fefc78aa8
Daiderd Jordan <djordan@openmotics.com>
2022-04-28 17:19:29 +0200

    add status calls to sensor/ventilation plugin sdk

--- src/plugin_runtime/sdk.py
+++ src/plugin_runtime/sdk.py
@@ -1,9 +1,10 @@
 from __future__ import absolute_import
 
 import requests
+import ujson as json
 
-from gateway.dto.sensor import SensorDTO
-from gateway.dto.ventilation import VentilationDTO
+from gateway.dto.sensor import SensorDTO, SensorStatusDTO
+from gateway.dto.ventilation import VentilationDTO, VentilationStatusDTO
 
 if False:  # MYPY
     from typing import Any, Dict
@@ -46,6 +47,16 @@ class SensorSDK(BaseSDK):
         data = self._api('POST', '/plugin/sensor/register', json=data)
         return SensorDTO(data['id'], external_id=external_id)
 
+    def set_status(self, sensor_id, value):
+        status = {'id': sensor_id, 'value': value}
+        # TODO replace with proper sensor api
+        data = self._api('POST', '/set_sensor_status', params={'status': json.dumps(status)})
+        if data.get('success', False) is True:
+            return SensorStatusDTO(data['status']['id'],
+                                   value=data['status']['value'])
+        else:
+            return None
+
 
 class VentilationSDK(BaseSDK):
     def register(self, external_id, config=None):
@@ -55,3 +66,27 @@ class VentilationSDK(BaseSDK):
                 'config': config or {}}
         data = self._api('POST', '/plugin/ventilation/register', json=data)
         return VentilationDTO(data['id'], external_id=external_id)
+
+    def set_auto(self, ventilation_id, **kwargs):
+        return self.set_status(ventilation_id, VentilationStatusDTO.Mode.AUTO, **kwargs)
+
+    def set_manual(self, ventilation_id, **kwargs):
+        return self.set_status(ventilation_id, VentilationStatusDTO.Mode.MANUAL, **kwargs)
+
+    def set_status(self, ventilation_id, mode, level=None, timer=None, remaining_time=None):
+        status = {'id': ventilation_id, 'mode': mode}
+        for key, value in [('level', level),
+                           ('timer', timer),
+                           ('remaining_time', remaining_time)]:
+            if value is not None:
+                status.update({key: value})
+        # TODO replace with proper ventilation api
+        data = self._api('POST', '/set_ventilation_status', params={'status': json.dumps(status)})
+        if data.get('success', False) is True:
+            return VentilationStatusDTO(data['status']['id'],
+                                        mode=data['status']['mode'],
+                                        level=data['status']['mode'],
+                                        timer=data['status']['timer'],
+                                        remaining_time=data['status']['remaining_time'])
+        else:
+            return None
